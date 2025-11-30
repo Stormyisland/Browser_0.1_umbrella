d694d9cd74d4aec7f236d7fb6bb612627a4107bd
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-11-30 15:20:52 -0800

    Implement Browser class with QWebEngineView
    Added a Browser class with a web engine view.

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtGui import QIcon, QKeySequence
+
+class Browser(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        # Create the web engine view
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.google.com"))
